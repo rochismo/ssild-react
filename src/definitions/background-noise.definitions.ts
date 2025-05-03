084b80a996c3b065864b00c1dbdd1f2c212d02c1
cardoben <benjamin.cardona@effem.com>
2025-05-03 09:11:02 +0200

    feat: :sparkles: Add background noise definitions

---
+++ src/definitions/background-noise.definitions.ts
@@ -0,0 +1,86 @@
+export const backgroundNoises = [
+  {
+    slug: 'aircon',
+    displayName: 'Air Conditioner',
+  },
+  {
+    slug: 'birds',
+    displayName: 'Birds',
+  },
+  {
+    slug: 'brownnoise',
+    displayName: 'Brown Noise',
+  },
+  {
+    slug: 'chimesmetal',
+    displayName: 'Metallic Chimes',
+  },
+  {
+    slug: 'cicadas',
+    displayName: 'Cicadas',
+  },
+  {
+    slug: 'city',
+    displayName: 'City Noise',
+  },
+  {
+    slug: 'crickets',
+    displayName: 'Crickets',
+  },
+  {
+    slug: 'fanhigh',
+    displayName: 'Fan at High Speed',
+  },
+  {
+    slug: 'fanlow',
+    displayName: 'Fan at Low Speed',
+  },
+  {
+    slug: 'fire',
+    displayName: 'Crackling Fire',
+  },
+  {
+    slug: 'frogs',
+    displayName: 'Frogs',
+  },
+  {
+    slug: 'people',
+    displayName: 'People Chatter',
+  },
+  {
+    slug: 'pinknoise',
+    displayName: 'Pink Noise',
+  },
+  {
+    slug: 'raincabin',
+    displayName: 'Rain Cabin',
+  },
+  {
+    slug: 'raintinroof',
+    displayName: 'Rain on tin roof',
+  },
+  {
+    slug: 'raintrees',
+    displayName: 'Rain on trees',
+  },
+  {
+    slug: 'stream',
+    displayName: 'Water Stream',
+  },
+  {
+    slug: 'vinyl',
+    displayName: 'Vinyl',
+  },
+  {
+    slug: 'waterfall',
+    displayName: 'Waterfall',
+  },
+  {
+    slug: 'waves',
+    displayName: 'Ocean Waves',
+  },
+  {
+    slug: 'wind',
+    displayName: 'Wind',
+  },
+]
