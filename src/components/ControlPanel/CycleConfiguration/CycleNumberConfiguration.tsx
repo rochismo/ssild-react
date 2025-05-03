f182594ad2cf5e584080ee0bfa25923319a6df82
cardoben <benjamin.cardona@effem.com>
2025-05-03 14:17:39 +0200

    fix: :bug: Fix issue not allowing to change the cycles input

--- src/components/ControlPanel/CycleConfiguration/CycleNumberConfiguration.tsx
+++ src/components/ControlPanel/CycleConfiguration/CycleNumberConfiguration.tsx
@@ -1,29 +1,39 @@
-import { MAX_CYCLES, MIN_CYCLES } from '@/constants/SSILD_CONSTANTS'
+import { DEFAULT_START_DELAY, MAX_CYCLES, MAX_START_DELAY, MIN_CYCLES } from '@/constants/SSILD_CONSTANTS'
 import { useSSILDContext } from '@/contexts/SSILDContext'
 import { Checkbox, Flex, NumberInput } from '@chakra-ui/react'
 
 export const CycleNumberConfiguration = () => {
   const { form, isRunning } = useSSILDContext()
   return (
-    <Flex alignContent={'space-between'} gap="5">
-      <NumberInput.Root
-        disabled={form.values.unlimited || isRunning}
-        onValueChange={({ valueAsNumber: value }) => form.changeValue('numberOfCycles', value)}
-      >
-        <NumberInput.Label>Number of Cycles</NumberInput.Label>
-        <NumberInput.Input value={form.values.numberOfCycles} min={MIN_CYCLES} max={MAX_CYCLES}></NumberInput.Input>
-      </NumberInput.Root>
+    <Flex direction="column" gap="6">
+      <Flex alignContent={'space-between'} gap="5">
+        <NumberInput.Root
+          disabled={form.values.unlimited || isRunning}
+          value={`${form.values.numberOfCycles}`}
+          onValueChange={({ valueAsNumber: value }) => form.changeValue('numberOfCycles', value || 0)}
+        >
+          <NumberInput.Label>Number of Cycles</NumberInput.Label>
+          <NumberInput.Input min={MIN_CYCLES} max={MAX_CYCLES}></NumberInput.Input>
+        </NumberInput.Root>
 
-      <Checkbox.Root
-        checked={form.values.unlimited}
+        <Checkbox.Root
+          checked={form.values.unlimited}
+          disabled={isRunning}
+          onCheckedChange={(x) => form.changeValue('unlimited', x.checked as boolean)}
+        >
+          <Checkbox.HiddenInput />
+          <Checkbox.Control />
+          <Checkbox.Label>Unlimited Cycles</Checkbox.Label>
+        </Checkbox.Root>
+      </Flex>
+      <NumberInput.Root
         disabled={isRunning}
-        defaultChecked={false}
-        onCheckedChange={(x) => form.changeValue('unlimited', x.checked as boolean)}
+        value={`${form.values.startDelay}`}
+        onValueChange={({ valueAsNumber: value }) => form.changeValue('startDelay', value || 0)}
       >
-        <Checkbox.HiddenInput />
-        <Checkbox.Control />
-        <Checkbox.Label>Unlimited Cycles</Checkbox.Label>
-      </Checkbox.Root>
+        <NumberInput.Label>Delay Start (sec)</NumberInput.Label>
+        <NumberInput.Input min={DEFAULT_START_DELAY} max={MAX_START_DELAY}></NumberInput.Input>
+      </NumberInput.Root>
     </Flex>
   )
 }
